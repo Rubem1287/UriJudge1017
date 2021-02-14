d92478e256a53be6f221449ca02fd6d9350b5a87
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:57:46 -0300

    Add files via upload

---
+++ Uri 1017/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            double valor;
+
+            
+
+           
+
+            Console.WriteLine();
+            Console.Write("Digite o tempo gasto na viagem : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite a velocidade média : ");
+            b = int.Parse(Console.ReadLine());
+
+             valor = (a * b) / 12.0;
+            
+
+
+
+            Console.WriteLine();
+            Console.Write("Quantidade de litros necessário para realizar a viagem : "+valor.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
